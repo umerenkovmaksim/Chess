b37dbc930641b843a0e606d83ceaab2313d9bf94
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2022-04-03 22:46:18 +0300

    1

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-from constans import *
-from Board import Board
+from solution import *
 
+WHITE=1
+BLACK=2
 
-def print_board(board):  # Распечатать доску в текстовом виде (см. скриншот)
-    print('     +----+----+----+----+----+----+----+----+')
-    for row in range(7, -1, -1):
-        print(' ', row, end='  ')
-        for col in range(8):
-            print('|', board.cell(row, col), end=' ')
-        print('|')
-        print('     +----+----+----+----+----+----+----+----+')
-    print(end='        ')
+row0 = 4
+col0 = 5
+queen = Queen(row0, col0, BLACK)
+
+print('white' if queen.get_color() == WHITE else 'black')
+for row in range(7, -1, -1):
     for col in range(8):
-        print(col, end='    ')
+        if row == row0 and col == col0:
+            print(queen.char(), end='')
+        elif queen.can_move(row, col):
+            print('x', end='')
+        else:
+            print('-', end='')
     print()
-
-
-def main():
-    # Создаём шахматную доску
-    board = Board()
-
-    # Цикл ввода команд игроков
-    while True:
-        # Выводим положение фигур на доске
-        print_board(board)
-        # Подсказка по командам
-        print('Команды:')
-        print('    exit                               -- выход')
-        print('    move <row> <col> <row1> <col1>     -- ход из клетки (row, col)')
-        print('                                          в клетку (row1, col1)')
-        # Выводим приглашение игроку нужного цвета
-        if board.current_player_color() == WHITE:
-            print('Ход белых:')
-        else:
-            print('Ход черных:')
-        command = input()
-        if command == 'exit':
-            break
-        move_type, row, col, row1, col1 = command.split()
-        row, col, row1, col1 = int(row), int(col), int(row1), int(col1)
-        if board.move_piece(row, col, row1, col1):
-            print('Ход успешен')
-        else:
-            print('Координаты некорректы! Попробуйте другой ход!')
-
-main()
-
